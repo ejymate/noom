3e6a742af74aa89277769000e4637be86e7413de
Eungjin Lee <dmdwls0208@gmail.com>
2024-01-13 10:21:44 +0900

    Pratice Nickname Process

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,46 @@
-alert("hi");
+var messageList = document.querySelector("ul");
+var nickForm = document.querySelector("#nick");
+var messageForm = document.querySelector("#message");
+
+// Frontend의 socket (서버와 연결)
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+    console.log("Connected to Server");
+});
+
+socket.addEventListener("message", (message) => {
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
+});
+
+socket.addEventListener("close", () => {
+    console.log("disconnected to Server");
+});
+
+function makeMessage(type, payload) {
+    const msg = {type, payload};
+    return JSON.stringify(msg);
+}
+
+function handleNickForm(event) {
+    event.preventDefault();
+
+    const input = nickForm.querySelector("input");
+    const spnNickName = nickForm.querySelector("#spnNickname");
+    socket.send(makeMessage("nickname", input.value));
+    spnNickName.innerText = input.value;
+    input.value = "";
+}
+
+function handleForm(event) {
+    event.preventDefault();
+
+    const input = messageForm.querySelector("input");
+    socket.send(makeMessage("new_message", input.value));
+    input.value = "";
+}
+
+nickForm.addEventListener("submit", handleNickForm);
+messageForm.addEventListener("submit", handleForm);
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import WebSocket from "ws";
+import http from "http";
 import express from "express";
 
 const app = express();
@@ -10,7 +12,40 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 
 // setting route
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+// app.listen(3000, handleListen);
+
+// http 내장 함수를 통해 서버를 만들고
+// Websocket 서버에 바이딩 시킨다
+// => 같은 location 과 같은 포트에서 http 와 ws 프로토콜을 사용할 수 있다.
+const server = http.createServer(app); 
+const wss = new WebSocket.Server({ server });
+
+const sockets = [];
+
+// backend의 socket (브라우저와 연결)
+wss.on("connection", (socket) => {
+    console.log("Connected to Browser");
+    
+    sockets.push(socket);
+    socket["nickname"] = "Annon";
+    
+    socket.on("close", () => console.log("Disconnected from the Browser"));
+    socket.on("message", (data) => {
+        const jsonMessage = JSON.parse(data);
+
+        switch(jsonMessage.type) {
+            case "new_message":
+                sockets.forEach((aSocket) => 
+                    aSocket.send(`${socket.nickname}: ${jsonMessage.payload}`)
+                );
+            case "nickname":
+                socket["nickname"] = jsonMessage.payload;
+        }
+    });
+});
+
+server.listen(3000, handleListen);
